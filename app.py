67b1ed417ea8b200c3bd5414d4909a918dc8ce5c
juanfirka14 <juanfirka14@gmail.com>
2026-03-07 13:28:57 +0000

    6

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-import motor_fisico
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import json
-import copy
-import re
+from motor_fisico import *
 
 # ==========================================
 # 3️⃣ ENTORNO VISUAL (INTERFAZ)
